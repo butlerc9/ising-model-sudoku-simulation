e4c4c6a57770180c830f4f55c0025ed67e9fff53
butlerc9 <33984091+butlerc9@users.noreply.github.com>
2017-12-26 23:22:36 +0000

    Animation of Random Matrix with Random Spin Flips Implemented

--- IsingModelTest.py
+++ IsingModelTest.py
@@ -9,26 +9,50 @@ from randommatrixgenerator import LatticeMaker
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import sys
+import matplotlib as mpl
 
+
+### Assignment code for Tom Achers PY3P01 Assignment
+### Creates a Ising Model Simulation Using Metropolis Algorithm
+###
+### USE: Keep sweep amount (s) at least 100 or program will run painfully slow
+### MUST IMPORT "randommatrixgenerator.py" as a package. Almost all Functions used are methods of Lattice class
+###
 
 
 """ Defining Constants """
 
-n = 128#n is the square lattice size
-
+n = 80 #n is the square lattice size
+s = 10000 #steps per picture update
+T = 3. #Temperature
 
 """ Testing Code """
 
-Lattice = LatticeMaker(n)
-Lattice.PrintLattice()
-Lattice.RanSpinChange()
-Lattice.RanSpinChange()
-Lattice.RanSpinChange()
-Lattice.PrintLattice()
-
-
-im = plt.imshow(Lattice.ReturnLattice(), animated=True,cmap = 'cool',interpolation = 'bilinear')
-
-
+Lattice = LatticeMaker(n,s) #creates new lattice instance and names it lattice
 
 """Making Graphs"""
+
+fig = plt.figure() #creates new figure
+ani = animation.FuncAnimation(fig, Lattice.RanSpinChange, interval=0.0001,blit='True')
+ax = fig.add_subplot(1, 1, 1)
+
+ax.set_title("Ising Model Simulation", fontsize='large') #setting title
+
+
+for tick in ax.xaxis.get_ticklabels(): #format x axis
+    tick.set_fontsize('large')
+    tick.set_fontname('Times New Roman')
+    tick.set_color('blue')
+    tick.set_weight('bold')
+
+for tick in ax.yaxis.get_ticklabels(): #format y axis
+    tick.set_fontsize('large')
+    tick.set_fontname('Times New Roman')
+    tick.set_color('blue')
+    tick.set_weight('bold')
+
+plt.xlabel('x-axis') #xaxis label
+plt.ylabel('y-axis') #yaxis label
+plt.show()
--- randommatrixgenerator.py
+++ randommatrixgenerator.py
@@ -5,30 +5,42 @@ Created on Fri Dec 22 16:43:59 2017
 @author: Cormac
 """
 
+
 """Importing Packages"""
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib as mpl
+import sys
 
 """Creating New Lattice Class"""
 
 class LatticeMaker: #LatticeMaker Class
-    def __init__(self,n): #n = size of matrix
-        self.n = n
-        self.matrix = np.random.randint(2, size=(self.n,self.n))*2 -1 #object has attribute matrix which defines the spin lattice
     
-    def RanSpinChange(self): #flips spin of random paticle. i is dummy variable 
-        x = random.randint(0,self.n-1) #random x value
-        y = random.randint(0,self.n-1) #random y value
-        self.matrix[x,y] *= -1 #flips random x,y coordinate
+    def __init__(self,n,s): #n = size of matrix
+        self.n = n #assigns lattice size
+        self.s = s #assigns number of steps per picture update
+        self.matrix = np.random.randint(2, size=(self.n,self.n))*2 -1 #generates random matrix filled with 0,1. Doubles then -1 to get +-1 matrix
+
+    
+    def RanSpinChange(self,i): #flips spin of random paticle. i is dummy variable
+        for m in range(1,self.s): #loop step number times
+            x = random.randint(0,self.n-1) #random x value
+            y = random.randint(0,self.n-1) #random y value
+            self.matrix[x,y] *= -1 #flips random x,y coordinate
+        self.image = plt.imshow(self.matrix,cmap = 'jet',interpolation = 'nearest',origin='lower') #image of lattice is updated as matrix.
+        return self.image, #image of lattice is returned to be in animation function
     
     def ReturnLattice(self): #returns matrix
         return self.matrix
     
     def PrintLattice(self): #prints matrix
         print self.matrix
-
+    
+    
 """Example"""
 #LatticeNew = LatticeMaker(3) #creates new lattice type with n = 10
 #LatticeNew.PrintLattice()
 #RandomMatrix = LatticeNew.RandomLattice() #generates random matrix from n value
+
