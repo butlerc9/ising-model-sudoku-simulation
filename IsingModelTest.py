01edc954f4e35971327c4b1aacd4904e8bacf13b
butlerc9 <33984091+butlerc9@users.noreply.github.com>
2017-12-27 23:37:11 +0000

    Checking of adjacent spins of random point implemented. Periodic Boundaries Added. Matrix Element Finding bug fixed

--- IsingModelTest.py
+++ IsingModelTest.py
@@ -12,25 +12,30 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import sys
 import matplotlib as mpl
+import math
 
 
 ### Assignment code for Tom Achers PY3P01 Assignment
 ### Creates a Ising Model Simulation Using Metropolis Algorithm
 ###
 ### USE: Keep sweep amount (s) at least 100 or program will run painfully slow
-### MUST IMPORT "randommatrixgenerator.py" as a package. Almost all Functions used are methods of Lattice class
-###
+### MUST IMPORT "randommatrixgenerator.py" as a package. Almost all Functions 
+### used are methods of Lattice class
+### Close Figures to open future ones to prevent crashes
 
+  
 
 """ Defining Constants """
 
-n = 80 #n is the square lattice size
-s = 10000 #steps per picture update
-T = 3. #Temperature
+n = 12 #n is the square lattice size
+s = int(math.ceil((float(n**2))/4)) #steps per picture update. 
+                                    #Scales to be quarter of diagram. Never zero.
+T = 3 #Temperature
+
 
 """ Testing Code """
 
-Lattice = LatticeMaker(n,s) #creates new lattice instance and names it lattice
+Lattice = LatticeMaker(n,s,T) #creates new lattice instance and names it lattice
 
 """Making Graphs"""
 
@@ -40,7 +45,7 @@ ax = fig.add_subplot(1, 1, 1)
 
 ax.set_title("Ising Model Simulation", fontsize='large') #setting title
 
-
+###formats axis labels to make them look better
 for tick in ax.xaxis.get_ticklabels(): #format x axis
     tick.set_fontsize('large')
     tick.set_fontname('Times New Roman')
@@ -56,3 +61,4 @@ for tick in ax.yaxis.get_ticklabels(): #format y axis
 plt.xlabel('x-axis') #xaxis label
 plt.ylabel('y-axis') #yaxis label
 plt.show()
+
--- randommatrixgenerator.py
+++ randommatrixgenerator.py
@@ -17,20 +17,30 @@ import sys
 """Creating New Lattice Class"""
 
 class LatticeMaker: #LatticeMaker Class
-    
-    def __init__(self,n,s): #n = size of matrix
+    def __init__(self,n,s,T): #n = size of matrix
         self.n = n #assigns lattice size
         self.s = s #assigns number of steps per picture update
+        self.T = T #assigns temperature of lattice
         self.matrix = np.random.randint(2, size=(self.n,self.n))*2 -1 #generates random matrix filled with 0,1. Doubles then -1 to get +-1 matrix
 
-    
     def RanSpinChange(self,i): #flips spin of random paticle. i is dummy variable
-        for m in range(1,self.s): #loop step number times
-            x = random.randint(0,self.n-1) #random x value
-            y = random.randint(0,self.n-1) #random y value
-            self.matrix[x,y] *= -1 #flips random x,y coordinate
+        for m in range(0,self.s): #loop step number times
+            row = random.randint(0,self.n-1) #random row value
+            col = random.randint(0,self.n-1) #random col value
+            self.matrix[col,row] *= -1 #flips random row,col coordinate
         self.image = plt.imshow(self.matrix,cmap = 'jet',interpolation = 'nearest',origin='lower') #image of lattice is updated as matrix.
         return self.image, #image of lattice is returned to be in animation function
+    
+    def CheckSpins(self,i):
+        row = random.randint(0,self.n-1) #generates random row value
+        col = random.randint(0,self.n-1) #generates random col value
+        
+        right = self.matrix[col,(row + 1) % self.n] #right = spin to right of random point in lattice
+        left = self.matrix[col,(row - 1) % self.n] #left = spin to left of random point in lattice etc.
+        up = self.matrix[(col - 1) % self.n,row] #Perioic Boundary conditions implemented. If point=lattice size
+        down = self.matrix[(col + 1) % self.n,row] #this is then assigned 0. l[-1] is default last in list
+        
+        spinsum = right+left+up+down #spinsum is the sum of all adjacent spins from the random spin
     
     def ReturnLattice(self): #returns matrix
         return self.matrix
